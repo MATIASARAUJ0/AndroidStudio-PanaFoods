0d6699be47b4cae94798119e83d20584602f7e95
MATIASARAUJ0 <araujovmatias@gmail.com>
2022-12-23 15:57:59 -0500

    NEW

---
+++ app/src/main/java/com/project1/panafoods/IColaborador.java
@@ -0,0 +1,7 @@
+package com.project1.panafoods;
+
+import com.project1.panafoods.Entities.Colaborador;
+
+public interface IColaborador {
+    public void seleccionarColaborador(Colaborador c);
+}
---
+++ app/src/main/java/com/project1/panafoods/IEncargado.java
@@ -0,0 +1,7 @@
+package com.project1.panafoods;
+
+import com.project1.panafoods.Entities.Encargado;
+
+public interface IEncargado {
+    public void seleccionarEncargado(Encargado e);
+}
